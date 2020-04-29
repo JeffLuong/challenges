bb9140419182f1a0dea44ef53922d053dcb6f283
Jeffrey Luong <jjck1988@gmail.com>
2020-04-29 10:41:38 -0400

    Convert time-to-complete functions to JobsManager class

--- time-to-complete.ts
+++ time-to-complete.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env ts-node
 /**
  * Given a list of jobs, find the total time it will take for it to be complete.
  * Each job will have an ID, a time to complete and a list of child job IDs:
@@ -18,8 +19,10 @@
  *
  */
 
+type Jobs = [number, number, number[]][];
+
 // Input
-const jobs: [number, number, number[]][] = [
+const jobs: Jobs = [
   [1, 30, [2, 4]],
   [2, 10, [3]],
   [4, 60, []],
@@ -32,44 +35,62 @@ type JobMap = {
   [id: string]: Job
 }
 
-/**
- * Recursively gets the times of all child jobs of a particular job while
- * also including the job's own time.
- * @param [time, children]
- * @param index map of jobs
- */
+class JobsManager {
+  public jobs: JobMap = {};
 
-function getTimes([time, children]: Job, index: JobMap): number[] {
-  if (!children.length) {
-    return [time];
+  private createJobsMap(jobs: Jobs): JobMap {
+    return jobs.reduce((acc: JobMap, [id, ...rest]): JobMap => {
+      const _id = id.toString();
+      if (!acc[_id]) {
+        return {
+          ...acc,
+          [_id]: rest
+        };
+      }
+      return acc;
+    }, {});
   }
-  return [
-    time,
-    ...children.reduce((acc: number[], child: number): number[] => {
-      const job = index[child];
-      return job ? [...acc, ...getTimes(index[child], index)] : acc;
-    }, [])
-  ];
-}
 
-/**
- * Creates a mapping of the jobs, then using the jobId arg, look up the job
- * with that ID then get all the times associated with it.
- * @param jobId
- */
+  constructor(jobs: Jobs) {
+    this.jobs = this.createJobsMap(jobs);
+  }
 
-function getTimesById(jobId: number): number {
-  const jobIndex = jobs.reduce((acc: JobMap, [id, ...rest]): JobMap => {
-    const _id = id.toString();
-    if (!acc[_id]) {
-      return {
-        ...acc,
-        [_id]: rest
-      };
+  /**
+   * Recursively gets the times of all child jobs of a particular job while
+   * also including the job's own time.
+   * @param [time, children]
+   * @param index map of jobs
+   */
+
+  getTimes([time, children]: Job): number[] {
+    if (!children.length) {
+      return [time];
     }
-    return acc;
-  }, {});
-  return jobIndex[jobId] ?
-    getTimes(jobIndex[jobId], jobIndex).reduce((a, c) => a += c, 0) :
-    0;
+    return [
+      time,
+      ...children.reduce((acc: number[], child: number): number[] => {
+        const job = this.jobs[child];
+        return job ? [...acc, ...this.getTimes(job)] : acc;
+      }, [])
+    ];
+  }
+
+  /**
+   * Look up the job with that ID then get all the times associated with it.
+   * @param id
+   */
+  getTimeToCompleteById(id: number | string): number {
+    return this.jobs[id] ?
+      this.getTimes(this.jobs[id]).reduce((a, c) => a += c, 0) :
+      0;
+  }
 }
+
+const manager = new JobsManager(jobs);
+
+console.log(`
+  Job 1: ${manager.getTimeToCompleteById(1)}
+  Job 2: ${manager.getTimeToCompleteById(2)}
+  Job 3: ${manager.getTimeToCompleteById(3)}
+  Job 4: ${manager.getTimeToCompleteById(4)}
+`);
