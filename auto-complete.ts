9236759f5dfcc15d22c56f7b4f194a5e83bee374
Jeffrey Luong <jjck1988@gmail.com>
2020-04-29 16:53:07 -0400

    Convert auto-complete to class

--- auto-complete.ts
+++ auto-complete.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env ts-node
+
 import { isObject } from './utils/object';
 
 /**
@@ -12,89 +14,112 @@ import { isObject } from './utils/object';
  * available words.
  */
 
-type Dictionary = {
-  // Need to better type this trie :(
-  [letter: string]: any,
- } | any;
+class Node {
+  public end: boolean;
+  public char?: string;
+  public children: Map<string, Node>;
 
-const dictionary: Dictionary = {};
-
-/**
- * Adds a string to the dictionary
- * @param str
- * @param dic
- */
-
-function addToDictionary(str: string, dic: Dictionary): Dictionary {
-  const arr = str.split('');
-  let currNode = dic;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (!currNode[arr[i]]) {
-      currNode[arr[i]] = (i === arr.length - 1) ? 1 : {};
-    } else if (!isObject(currNode[arr[i]])) {
-      currNode[arr[i]] = { end: 1 };
-    } else if (isObject(currNode[arr[i]]) && i === arr.length - 1) {
-      currNode[arr[i]] = { end: 1, ...(currNode[arr[i]] as object) };
-    }
-    currNode = currNode[arr[i]];
-  }
-
-  return dic;
-}
-
-/**
- * Takes a node and an accumulated string (if any) and finds all leaves for the node,
- * diving deeper into the trie in order to pull out the possible words - concatanating
- * them with the initla accumulated string.
- * @param str
- * @param dic
- */
-
-function dive(node: Dictionary, str: string = ''): string[] {
-  if (!node || node === 1) {
-    return [str];
-  }
-  const letters = Object.keys(node).filter(k => k !== 'end');
-  const res: string[] = [];
-  letters.forEach(l => {
-    const args = [...dive(node[l], `${str}${l}`)];
-    if (node.end) {
-      args.unshift(`${str}`);
-    }
-    res.push(...args);
-  });
-  return res;
-}
-
-/**
- * Takes an initial string and follows the letters into the trie. If the string
- * exists, dive into the latest node to get all possibilities.
- * @param str
- * @param dic
- */
-function search(str: string, dic: Dictionary): string[] {
-  const arr = str.split('');
-
-  if (!arr.length || !dic) {
-    return [];
-  }
-
-  let currNode = dic;
-
-  for (let i = 0; i < arr.length; i++) {
-    const node = currNode[arr[i]];
-    if (!node) {
-      break;
-    } else if (isObject(node)) {
-      currNode = node;
-    } else if (node === 1) {
-      currNode = null;
+  constructor(char?: string) {
+    this.end = false;
+    this.children = new Map<string, Node>();
+    if (char) {
+      this.char = char;
     }
   }
-  return [...dive(currNode, str)];
 }
 
-function autoComplete(str: string): string[] {
-  return search(str, dictionary);
+class Trie {
+  private root: Node;
+
+  constructor() {
+    this.root = new Node();
+  }
+
+  /**
+   * Finds the node based on a list of strings, split from the word.
+   * @param charArray
+   * @param node
+   */
+  private findNode([char, ...rest]: string[], node: Node = this.root): Node | null {
+    if (char === undefined) {
+      return node;
+    }
+
+    const { children } = node;
+    const nextNode = children.get(char);
+
+    if (!nextNode) {
+      return null;
+    }
+
+    return this.findNode(rest, nextNode);
+  }
+
+  /**
+   * Gets all possible words from the Trie from the node argument.
+   * Also takes a prefix string to join with the possibilties.
+   * @param node
+   * @param str
+   */
+  private getPossibleOptions(node: Node, str: string): string[] {
+    if (node.children.size === 0) {
+      return [str];
+    }
+
+    const entries = node.children.entries();
+    const options: string[] = [];
+
+    for (let i = 0; i < node.children.size; i++) {
+      const [char, nextNode] = entries.next().value;
+      const args = [...this.getPossibleOptions(nextNode, `${str}${char}`)];
+      if (node.end) {
+        args.unshift(str);
+      }
+      options.push(...args);
+    };
+
+    return options;
+  }
+
+  /**
+   * Add a word to the Trie dictionary
+   * @param word
+   */
+  public add(word: string): void {
+    let children = this.root.children;
+    let letterIdx = 0;
+
+    for (const letter of word) {
+      let node = children.get(letter);
+      if (!node) {
+        node = new Node(letter);
+        children.set(letter, node);
+      }
+
+      children = node.children;
+      letterIdx += 1;
+
+      if (letterIdx === word.length) {
+        node.end = true;
+      }
+    }
+  }
+
+  /**
+   * Returns the potential options from a given string.
+   * @param word
+   */
+  public autoComplete(word: string): string[] {
+    const node = this.findNode(word.split(''));
+    return node && this.getPossibleOptions(node, word) || [];
+  }
 }
+
+const dictionary = new Trie();
+
+dictionary.add('apply');
+dictionary.add('apple');
+dictionary.add('application');
+dictionary.add('approach');
+
+console.log(dictionary.autoComplete('app'));
