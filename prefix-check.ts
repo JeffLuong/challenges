0f649a1257f04b69efa7f1909f72fb98495b7380
Jeffrey Luong <jjck1988@gmail.com>
2020-05-25 13:11:57 -0400

    Add prefix-check challenge

---
+++ prefix-check.ts
@@ -0,0 +1,106 @@
+/**
+ * Given an array of strings, find the longest available prefix that
+ * is present in each. If there is no prefix available, return an empty string
+ * 
+ * Examples:
+ *
+ * Input
+ * ['abcd', 'abcep', 'abcqdf', 'abcdd']
+ *
+ * Output
+ * 'abc'
+ *
+ * Input
+ * ['eba', 'ae', 'aeon', 'aeape', 'abee']
+ * 
+ * Output
+ * ''
+ *
+ * Input
+ * ['efg', 'efgh', 'efghi', 'efghij', '']
+ *
+ * Output
+ * ''
+ *
+ */
+
+// Greedy solution
+const greedyPrefixCheck: (s: string[]) => string = strings => {
+  if (strings.length === 0) {
+    return '';
+  }
+
+  let prefix = strings.shift();
+
+  for (const string of strings) {
+    if (string.length === 0) {
+      return '';
+    }
+
+    // Check if string includes prefix - example:
+    // skip if prefix = 'abcd' and string = 'abcde' <-- longest prefix is 'abcd'
+    // loop if prefix = 'abcd' and string = 'abc'   <-- loop through this to find shorter prefix
+    if (prefix && !string.includes(prefix)) {
+      let curr = '';
+      for (let i = 1; i <= string.length; i++) {
+        const sliced = string.slice(0, i);
+        if (prefix.startsWith(sliced)) {
+          curr = sliced;
+        } else {
+          break;
+        }
+      }
+      // If there is a shorter prefix available, set it.
+      // Otherwise return empty string.
+      if (curr) {
+        prefix = curr;
+      } else {
+        return '';
+      }
+    }
+  }
+
+  return prefix || '';
+};
+
+
+// Hash/Map solution
+const prefixCheckWithMap: (s: string[]) => string = strings => {
+  if (strings.length === 0) {
+    return '';
+  }
+
+  const map: { [key: string]: number } = {};
+
+  for (const string of strings) {
+    if (string.length === 0) {
+      return '';
+    }
+
+    const letters = string.split('');
+    let accumulated = '';
+
+    // Map out all available prefixes
+    for (const letter of letters) {
+      accumulated = `${accumulated}${letter}`;
+      if (map[accumulated]) {
+        map[accumulated] += 1;
+      } else {
+        map[accumulated] = 1;
+      }
+    }
+  }
+
+  // Get all counts and find highest
+  const counts = Object.values(map);
+  const highestCount = Math.max(...counts);
+
+  // If the highest count is less than the number of strings
+  // then not all of them have the same prefix.
+  if (highestCount !== strings.length) {
+    return '';
+  }
+
+  const highestCountIdx = counts.lastIndexOf(highestCount);
+  return Object.keys(map)[highestCountIdx];
+};
