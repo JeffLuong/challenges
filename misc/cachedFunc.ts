1906f335abba93ee64526bbaa4fa9425413c9faa
Jeffrey Luong <jjck1988@gmail.com>
2020-06-03 22:41:42 -0400

    Add cachedFunc

---
+++ misc/cachedFunc.ts
@@ -0,0 +1,47 @@
+/**
+ * Create a function that caches (internally) any function returns to
+ * avoid executing the same function to get the same results. Example:
+ *
+ * function greeter(name) {
+ *   return `Hi, ${name}`;
+ * }
+ *
+ * const greetPeople = cachedFunc(greeter);
+ *
+ * 1st call:
+ * greetPeople('Mark') // => 'Hi Mark'
+ *
+ * 2nd call:
+ * greetPeople('Mark') // => 'Hi Mark' from cache
+ *
+ * @param fn 
+ */
+
+type CachedFunc = (args: any) => any;
+
+function cachedFunc(fn: Function): CachedFunc {
+  const cache: { [key: string]: any } = {};
+
+  return (...args: any[]) => {
+    // Tricky part is that there may be no arguments passed,
+    // resulting in '' (empty string) keys.
+    const key = args.slice().join('');
+
+    if (cache[key]) {
+      return cache[key];
+    }
+    cache[key] = fn(...args);
+    return cache[key];
+  }
+}
+
+function greeter(name: string) {
+  return `Hi, ${name}`;
+}
+
+const greetPeople = cachedFunc(greeter);
+
+greetPeople('Mark'); // => 'Hi, Mark'
+greetPeople('Erin'); // => 'Hi, Erin'
+greetPeople('Jeff'); // => 'Hi, Jeff'
+greetPeople('Elle'); // => 'Hi, Elle'
