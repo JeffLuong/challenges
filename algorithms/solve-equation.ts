f84f073536490e4cf2a474735eaefc355772dad7
Jeffrey Luong <jjck1988@gmail.com>
2020-06-04 22:40:59 -0400

    Improve solve-equation challenge even more

--- algorithms/solve-equation.ts
+++ algorithms/solve-equation.ts
@@ -17,10 +17,6 @@
 
 // O(n) solution
 const solveEquation = (c: number): boolean => {
-  // Cache the squared numbers until c
-  // The key is the diff between c - sqd (current sqd num)
-  const sqdDiffs: { [diff: number]: number } = {};
-
   // Find all squared values that are less than c
   for (let i = 0; i < c; i++) {
     const sqd = Math.pow(i, 2);
@@ -28,14 +24,9 @@ const solveEquation = (c: number): boolean => {
       break;
     }
     // Example: c = 13 and current sqd number = 4 that means diff is 9
-    // we cache { '9': 4 } so that when the current sqd num is 9
-    // we know the that the sum exists
-    const diff = c - sqd;
-
-    if (sqdDiffs[sqd] !== undefined) {
+    // Then check if sq root of diff (9) is a whole number.
+    if (Number.isInteger(Math.sqrt(c - sqd))) {
       return true;
-    } else {
-      sqdDiffs[diff] = sqd;
     }
   }
   return false;
