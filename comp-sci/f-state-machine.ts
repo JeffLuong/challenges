86dbc414ccd0b354e79bd5236b35a90f7fa8f243
Jeffrey Luong <jjck1988@gmail.com>
2020-06-09 09:25:43 -0400

    Add turn based game state machine

--- comp-sci/f-state-machine.ts
+++ comp-sci/f-state-machine.ts
@@ -4,35 +4,57 @@
  * cannot be changed unless calling the `transition()` method with an event passed
  * into it. This is to avoid any invalid states from occurring.
  *
+ * Examples:
+ *  // Define lightbulb state machine factory:
+ *  const createLightbulb = FMSFactory({
+ *    initialState: 'off',
+ *    states: {
+ *      off: {
+ *        ...
+ *      }
+ *      on: {
+ *        ...
+ *      }
+ *    }
+ *  });
+ *
+ *  const lightbulb = createLightbulb();  // create lightbulb from factory
+ *  lightbulb.getState();                 // 'off'
+ *  lightbulb.transition('switch');       // transition to next state
+ *  lightbulb.getState();                 // 'on'
+ *
  * @param definition 
  */
+
+type FSMState = {
+  actions: {
+    [action: string]: Function
+  },
+  transitions: {
+    [event: string]: {
+      target: string,
+      action: Function
+    }
+  }
+};
 
 type FSMDefinition = {
   initialState: string,
   states: {
-    [state: string]: {
-      actions: {
-        [action: string]: Function
-      },
-      transitions: {
-        [event: string]: {
-          target: string,
-          action: Function
-        }
-      }
-    }
+    [state: string]: FSMState
   }
 };
 
 type Machine = {
   state: string,
-  transition?: (event: string) => string,
+  transition: (event: string) => string,
   getState?: () => string
 }
 
 function FSMFactory(definition: FSMDefinition) {
   const machine: Machine = {
-    state: 'None'
+    state: 'None',
+    transition: (event: string) => ''
   };
 
   return function() {
@@ -47,11 +69,14 @@ function FSMFactory(definition: FSMDefinition) {
       const nextState = nextTransition.target;
       const nextTransitionDef = states[nextState];
 
-      currStateDef.actions.exit();
+      // End current state and prepare for next state.
+      currStateDef.actions.end();
       nextTransition.action();
-      nextTransitionDef.actions.enter();
 
+      // Set next state as new state.
       machine.state = nextState;
+      // Fire next state's start method.
+      nextTransitionDef.actions.start(machine, nextTransitionDef);
 
       return `Current state: ${machine.state}`;
     }
@@ -65,15 +90,15 @@ function FSMFactory(definition: FSMDefinition) {
   }
 }
 
-const lightbulbFactory = FSMFactory({
+const createLightbulb = FSMFactory({
   initialState: 'off',
   states: {
     on: {
       actions: {
-        enter() {
+        start() {
           console.log('Entering on...');
         },
-        exit() {
+        end() {
           console.log('Exiting on...');
         }
       },
@@ -88,10 +113,10 @@ const lightbulbFactory = FSMFactory({
     },
     off: {
       actions: {
-        enter() {
+        start() {
           console.log('Entering off...');
         },
-        exit() {
+        end() {
           console.log('Exiting off...');
         }
       },
@@ -104,5 +129,99 @@ const lightbulbFactory = FSMFactory({
         }
       }
     }
+  }
+});
+
+const actionTransitions = {
+  attack: {
+    target: 'offense',
+    action() {
+      console.log('Transitioning to offense...');
+    }
+  },
+  heal: {
+    target: 'defense',
+    action() {
+      console.log('Transitioning to defense...');
+    }
+  }
+};
+
+const endTurn = {
+  target: 'enemyTurn',
+  action() {
+    console.log('Transitioning to enemy turn...');
+  }
+};
+
+const turnBasedGameFactory = FSMFactory({
+  initialState: 'start',
+  states: {
+    start: {
+      actions: {
+        start() {
+          console.log('Entering start state...');
+        },
+        end() {
+          console.log('Exiting start state...');
+        }
+      },
+      transitions: {
+        ...actionTransitions
+      }
+    },
+    offense: {
+      actions: {
+        start(machine: Machine, { transitions }: FSMState) {
+          console.log('Preparing to attack...');
+          if (transitions.endTurn) {
+            machine.transition('endTurn');
+          }
+        },
+        end() {
+          console.log(`Hit enemy for ${Math.floor(Math.random() * 10)} points of damage!`);
+        }
+      },
+      transitions: {
+        endTurn
+      }
+    },
+    defense: {
+      actions: {
+        start(machine: Machine, { transitions }: FSMState) {
+          console.log('Entering defense state...');
+          if (transitions.endTurn) {
+            machine.transition('endTurn');
+          }
+        },
+        end() {
+          console.log(`Gained ${Math.floor(Math.random() * 10)} points of health from heal!`);
+        }
+      },
+      transitions: {
+        endTurn
+      }
+    },
+    enemyTurn: {
+      actions: {
+        start(machine: Machine, { transitions }: FSMState) {
+          console.log('Entering enemy turn state...');
+          if (transitions.endEnemyTurn) {
+            machine.transition('endEnemyTurn');
+          }
+        },
+        end() {
+          console.log(`Enemy attacked you for ${Math.floor(Math.random() * 10)} points of damage!`);
+        }
+      },
+      transitions: {
+        endEnemyTurn: {
+          target: 'start',
+          action() {
+            console.log('Transitioning to start state...');
+          }
+        }
+      }
+    },
   }
 });
